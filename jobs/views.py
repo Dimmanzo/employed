08cbd97b2903b3fdb7d4431e79a51122d65ca6c3
Dmitrij Sazniov <dmitrij.sazniov@gmail.com>
2024-09-22 08:38:13 +0000

    Added views for the job list

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Job
 
 # Create your views here.
-def index(request):
-    return render(request, 'jobs/index.html')
+class JobList(generic.ListView):
+    model = Job

99f8c9daa297fb0b206ce49cdc2af57138ee9285
Dmitrij Sazniov <dmitrij.sazniov@gmail.com>
2024-10-01 17:52:02 +0000

    Added stretch-link to make job block clickable, updated login page with register link, removed unfinished profile functionality

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,7 +4,6 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('profile/', profile_views.profile_view, name='profile'),
     path('register/', profile_views.register, name='register'),
     path('login/', profile_views.login_user, name='login'),
     path('logout/', profile_views.logout_user, name='logout'),
